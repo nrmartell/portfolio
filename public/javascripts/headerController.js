7c8d22786c2226f1995d9c8caba344ae9ac1c5ed
Natalie Martell <nrmartell@gmail.com>
2015-11-19 09:28:55 -0700

    little style edits

--- public/javascripts/headerController.js
+++ public/javascripts/headerController.js
@@ -2,9 +2,20 @@
 	var app = angular.module('MyPortfolio');
 
 	app.controller('headerController', function(){
-		this.title = "Natalie Martell Web Developer";
+		this.title = "*Natalie Martell Web Developer";
 		console.log(this.title)
 		return this;
 		console.log("thistitle");
 	});	
 })();
+
+// app.directive('backImg', function(){
+//     return function(scope, element, attrs){
+//         attrs.$observe('backImg', function(value) {
+//             element.css({
+//                 'background-image': 'url('/images/AdobeStock_54572760.gif')',
+//                 'background-size' : 'cover'
+//             });
+//         });
+//     };
+// });
